0a3d6bf7ff2e24c2da833dc7cb07cf15d6278752
jagdishgurram <gurramjagdish@gmail.com>
2026-06-10 00:18:14 +0530

    Updated

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -1,5 +1,4 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.sdk import dag, task
 from datetime import datetime,timedelta
 
 from etl.extract import extract_data
@@ -8,48 +7,47 @@ from etl.load import load_data
 from validation.validate import run_all_validations
 from etl.star import create_star_schema
 
-default_args = {
-    "owner": "jagdish",
-    "retries": 2,
-    "retry_delay": timedelta(minutes=5)
-}   
-
-with DAG(
+@dag(
     dag_id="nyc_taxi_pipeline",
-    start_date=datetime(2026,1,1),
+    start_date=datetime(2026, 1, 1),
     schedule=None,
-    catchup=False,
-    default_args=default_args
-) as dag:
-    
-    extract_task =  PythonOperator(
-        task_id = 'extract_data',
-        python_callable=extract_data,
-        dag=dag
-    )
-    
-    transform_task =  PythonOperator(
-        task_id = 'transform_data',
-        python_callable=transform_data,
-        dag=dag
-    )
-    
-    validate_task =  PythonOperator(
-        task_id = 'validate_data',
-        python_callable=run_all_validations,
-        dag=dag
-    )
-    
-    load_task =  PythonOperator(
-        task_id = 'load_data',
-        python_callable=load_data,
-        dag=dag
-    ) 
-    
-    star_task =  PythonOperator(
-        task_id = 'create_star_schema',
-        python_callable=create_star_schema,
-        dag=dag
-    )     
-    
-    extract_task >> transform_task >> validate_task >> load_task >> star_task
+    catchup=False
+)
+
+def pipeline_nyc():
+    @task.python
+    def extract_task():
+        extract_data()
+        return "Data Extraction Completed"
+
+    @task.python
+    def transform_task():
+        transform_data()
+        return "Data Transformation Completed"
+        
+
+    @task.python
+    def validate_task():
+        run_all_validations()
+        return "Data Validation Completed"
+
+    @task.python
+    def load_task():
+        load_data()
+        return "Data Loading Completed"
+
+    @task.python
+    def star_task():
+        create_star_schema()
+        return "Star Schema Created"
+
+    # Defining Dag Dependencies
+    extract = extract_task()
+    transform = transform_task()
+    validate = validate_task()
+    load = load_task()
+    star = star_task()
+
+    extract >> transform >> validate >> load >> star
+
+pipeline_nyc()
